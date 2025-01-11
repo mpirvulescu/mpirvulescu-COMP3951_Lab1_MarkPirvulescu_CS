d45d0fc4b9b428f33a60768e2e2e6970a4b82bee
markp <mark.pirvulescu@gmail.com>
2025-01-11 15:56:20 -0800

    Add project files.

---
+++ Lab1_MarkPirvulescu/Form1.cs
@@ -0,0 +1,36 @@
+using System;
+using System.Windows.Forms;
+
+namespace Lab1_MarkPirvulescu
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            label1.Text = "Enter the amount you want to invest";
+            label2.Text = "Enter the rate at which this index is supposed to grow at";
+            label3.Text = "Enter the amount of years of growth";
+            button1.Text = "Calculate!";
+        }
+
+        private void TextBox1_TextChanged(object sender, EventArgs e)
+        {
+
+        }
+
+        private void Label1_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void Button1_Click(object sender, EventArgs e)
+        {
+
+        }
+    }
+}
---
+++ Lab1_MarkPirvulescu/Investment.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Lab1_MarkPirvulescu
+{
+    public class Investment
+    {
+
+        private readonly double initialInvestment;
+        private readonly double rateOfGrowth;
+        private readonly double yearsOfGrowth;
+
+        public Investment(double initialInvestment, double rateOfGrowth, double yearsOfGrowth)
+        {
+            this.initialInvestment = initialInvestment;
+            this.rateOfGrowth = rateOfGrowth;
+            this.yearsOfGrowth = yearsOfGrowth;
+        }
+
+        private static void ValidateInitialInvestment(double initialInvestment)
+        {
+
+        }
+
+    }
+}
